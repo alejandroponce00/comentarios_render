10cae09073412e69822b4ae3b312fafaa4a884e3
Alejandro Ponce <122680586+alejandroponce00@users.noreply.github.com>
2024-04-04 19:27:34 -0300

    prep para railwin10

--- principal/settings.py
+++ principal/settings.py
@@ -132,4 +132,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 ALLOWED_HOSTS = ['localhost','comentariosrender-production.up.railway.app']
 
-CSRF_TRUSTED_ORIGINS = ['http://*','https://comentariosrender-production.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['http://*','https://comentariosrender-production.up.railway.app']
+                       
