18bff21a78dda4d9b87e3eb098ef585f805c86d2
Alejandro Ponce <122680586+alejandroponce00@users.noreply.github.com>
2024-04-04 19:00:52 -0300

    prep para railwin7

--- principal/settings.py
+++ principal/settings.py
@@ -128,6 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'caja_coment/static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 ALLOWED_HOSTS = ['localhost','comentariosrender-production.up.railway.app']
