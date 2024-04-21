d18c8923b327663823175e45952eb3e676daa338
Alejandro Ponce <122680586+alejandroponce00@users.noreply.github.com>
2024-04-21 19:02:38 -0300

    agregado muestra de comentarios

--- caja_coment/views.py
+++ caja_coment/views.py
@@ -1,24 +1,18 @@
 from django.shortcuts import render
 from .forms import CreateComentario
 from .models import Comentarios
+
 def caja_coment(request):
-    
-    form = CreateComentario
-    
+    form = CreateComentario()
+
     if request.method == "POST":
-           # print(request.POST['nombre'])
-           form = CreateComentario(request.POST)
-           
-           if form.is_valid():
-                   print("valido")
-                  
-                   comentarios= Comentarios()
-                   
-                   comentarios.nombre = form.cleaned_data['nombre']
-                   comentarios.mensaje = form.cleaned_data['mensaje']
-                  
-                   comentarios.save()
-           else:
-                   print("invalido")        
-            
-    return render(request, 'coment.html' ,{'form' : form}) 
+        form = CreateComentario(request.POST)
+        if form.is_valid():
+            nombre = form.cleaned_data['nombre']
+            mensaje = form.cleaned_data['mensaje']
+            Comentarios.objects.create(nombre=nombre, mensaje=mensaje)  # Crear un nuevo comentario en la base de datos
+
+    # Obtener todos los comentarios
+    comentarios = Comentarios.objects.all()
+
+    return render(request, 'coment.html', {'form': form, 'comentarios': comentarios})
